65dfb20e7e1d7821f76e32d9192797043789f27d
puckfried <puckfried@posteo.de>
2022-06-15 10:19:26 +0200

    solution added

---
+++ pb-funcII03-recur-sum.js
@@ -0,0 +1,12 @@
+const sum = function(array) {
+  if (array.length === 1) {
+    return array[0];
+  }
+  else {
+    return array.pop() + sum(array);
+  }
+};
+
+const array = [1,2,3,4,5,6]
+
+console.log(sum(array));
